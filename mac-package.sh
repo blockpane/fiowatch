eaf5d1146938c00b566f797d927be137539fd36a
Todd G <tag@frameloss.org>
2020-08-20 08:55:28 -0600

    fix package script

--- mac-package.sh
+++ mac-package.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
-# builds a macos package (.app) and places it inside a compressed .dmg
+export CGO_CFLAGS="-mmacosx-version-min=10.14"
+export CGO_LDFLAGS="-mmacosx-version-min=10.14"
+# builds a macos package (.app) and places it inside a .dmg
 
 mkdir -p "package/FIO Watch"
 mkdir -p package/old
 mv package/*.dmg package/old/
 
-#GOOS=darwin go build -ldflags "-s -w" -o cmd/contract-explorer/contract-explorer cmd/contract-explorer/main.go
 go build -ldflags "-s -w" -o cmd/fio-watch/fio-watch cmd/fio-watch/main.go
 
+# I've had mixed results with compressed .dmg images, some people have complained, compress the binary instead:
 upx -9 cmd/fio-watch/fio-watch
-fyne package -sourceDir cmd/contract-explorer -name "FIO Watch" -os darwin && mv "FIO Watch.app" "package/FIO watch/"
+
+fyne package -sourceDir cmd/fio-watch -name "FIO Watch" -os darwin && mv "FIO Watch.app" "package/FIO watch/"
 sed -i'' -e 's/.string.1\.0.\/string./\<string>'$(git describe --tags --always --long)'\<\/string>/g' "package/FIO watch/FIO watch.app/Contents/Info.plist"
+
 rm -f cmd/fio-watch/fio-watch
 pushd package
-ls -alh
-# hdiutil create -srcfolder "FIO contract-explorer" -format UDBZ "FIO contract-explorer.dmg"
-hdiutil create -srcfolder "FIO contract-explorer" "FIO contract-explorer.dmg"
+hdiutil create -srcfolder "FIO watch" "FIO watch.dmg"
 popd
-rm -fr "package/FIO contract-explorer"
-open "package/FIO contract-explorer.dmg"
+
+rm -fr "package/FIO fio-watch"
+open "package/FIO watch.dmg"
 
